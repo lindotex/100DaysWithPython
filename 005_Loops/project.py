e488b072b1e082f40ec77ba9b7e000a56e38c8a1
lindotex <lindoten@gmail.com>
2023-11-07 12:27:32 +0000

    Lesson 5: Password generator

---
+++ 005_Loops/project.py
@@ -0,0 +1,64 @@
+# Password Generator
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# My first try
+
+# array = []
+
+# for letters in range(0, nr_letters + 1):
+#     for symbols in range(0, nr_symbols + 1):
+#         for number in range(0, nr_numbers + 1):
+#             x = numbers.random()
+#             array.append(x)
+#     y = symbols.random()
+#     array.append(y)    
+# z = letters.random()
+# array.append(z)
+
+# array.random()
+# print(array)
+
+#Easy mode:
+
+# password = ""
+
+# for char in range(1, nr_letters+ 1):
+#     password += random.choice(letters)
+
+# for char in range(1, nr_symbols + 1):
+#     password += random.choice(symbols)
+    
+# for char in range(1, nr_numbers + 1):
+#     password += random.choice(numbers)
+
+# print(password)
+    
+#Hard Mode:
+password_list = []
+
+for char in range(1, nr_letters+ 1):
+    password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+    password_list.append(random.choice(symbols))
+    
+for char in range(1, nr_numbers + 1):
+    password_list.append(random.choice(numbers))
+
+random.shuffle(password_list)
+
+
+password = ""
+for char in password_list:
+    password += char
+    
+
+print(password)
