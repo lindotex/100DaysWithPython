b0a1a780698db4c451c8bcea97d82047e78d37b0
lindotex <lindoten@gmail.com>
2023-11-13 15:26:47 +0000

    lesson 8: ceasar cipher 1.0

--- 008_Functions_parameters/project.py
+++ 008_Functions_parameters/project.py
@@ -7,39 +7,30 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+#TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+#e.g. 
+#plain_text = "hello"
+#shift = 5
+#cipher_text = "mjqqt"
+#print output: "The encoded text is mjqqt"
 
-def encrypt(plain_text, shift_amount):
+##HINT: How do you get the index of an item in a list:
+#https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+def caesar(plain_text, shift_amount, direction):
     cipher_text = ''
     for letter in plain_text:
         position = alphabet.index(letter)
-        new_position = position + shift_amount
+        if direction == 'encode':
+            new_position = position + shift_amount
+        if direction == 'decode':
+            new_position = position - shift_amount
         new_letter = alphabet[new_position]
         cipher_text += new_letter
-    print(f"The encoded text is: {cipher_text}")
+    print(f"The {direction} text by {shift} shift number is:\n {cipher_text}")
+
+caesar(plain_text=text, shift_amount=shift, direction=direction)
  
-def decrypt(plain_text, shift_amount):
-    cipher_text = ''
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The decrypted text is: {cipher_text}")
-
-if(direction == 'encode'):
-    encrypt(plain_text=text, shift_amount=shift)
-if(direction == 'decode'):
-    decrypt(plain_text=text, shift_amount=shift)
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
