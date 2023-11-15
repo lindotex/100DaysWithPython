9ac23e82bf3f1624e1f6aa6de8f48944fc8ed05d
lindotex <lindoten@gmail.com>
2023-11-15 16:48:32 +0000

    lesson 9: Nesting dictionaries

--- 009_Dictionaries_Nesting/nesting.py
+++ 009_Dictionaries_Nesting/nesting.py
@@ -19,20 +19,28 @@ for key in range(0, len(travel_log["france"])):
 
 # Nesting a Dictionary into a Dictionary:
 
-travel_log = {
-    "france": {
-        "Paris": {
-            "visits": "2",
-        }, 
-        "Lille": {
-            "visits": "4",
-        }, 
-        "Dijon": {
-            "visits": "6",
-        }, 
-    },
-}
+travel_log = [
+  {
+    "country": "France",
+    "visits": 12,
+    "cities": ["Paris", "Lille", "Dijon"]
+  },
+  {
+    "country": "Germany",
+    "visits": 5,
+    "cities": ["Berlin", "Hamburg", "Stuttgart"]
+  },
+]
 
-print(travel_log["france"]["Paris"]["visits"])
-print(travel_log["france"]["Lille"]["visits"])
-print(travel_log["france"]["Dijon"]["visits"])
+def add_new_country(country, visit, list_of_cities):
+    new_country = {}
+    new_country["country"] = country
+    new_country["visit"] = visit
+    new_country["cities"] = list_of_cities
+    travel_log.append(new_country)
+
+add_new_country("brazil", 4, ["Rio de Janeiro", "Sao paulo", "Salvador", "Ilheus",])
+print(travel_log)
+
+print(f"I've been to {travel_log[2]["country"]} {travel_log[2]["visit"]} times ")
+print(f"My favorite city was {travel_log[2]["cities"][0]}")
