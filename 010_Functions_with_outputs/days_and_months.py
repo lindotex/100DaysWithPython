9ac1cfa2cc0ea9e44e803be32baabef56b6b721c
lindotex <lindoten@gmail.com>
2023-11-16 12:37:25 +0000

    lesson 10 : days and months finished

--- 010_Functions_with_outputs/days_and_months.py
+++ 010_Functions_with_outputs/days_and_months.py
@@ -1,22 +1,31 @@
 def is_leap(year):
-  if year % 4 == 0:
-    if year % 100 == 0:
-      if year % 400 == 0:
-        print("Leap year")
-      else:
-        print("Not leap year")
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
     else:
-      print("Leap year")
-  else:
-    print("Not leap year")
-  
-# TODO: Add more code here 👇
-def days_in_month():
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
+        return False
 
+
+
+# TODO: Add more code here 👇
+def days_in_month(year, days):
+    month_days =  [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
+    if month < 0 or month > 12:
+        return f"There is no month {month}... type a valid word."
+    if is_leap(year) == True and month == 2:
+        return 29
+    else:
+        return month_days[month -1 ]
+    
   
 #🚨 Do NOT change any of the code below 
-year = int(input()) # Enter a year
-month = int(input()) # Enter a month
+
+year = int(input("Type the Year:\n")) # Enter a year
+month = int(input("Type the month: \n")) # Enter a month
 days = days_in_month(year, month)
 print(days)
