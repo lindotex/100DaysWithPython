8567e5835fc707bd7b3515fb811568de9866b115
lindotex <lindoten@gmail.com>
2023-11-24 09:18:33 +0000

    lesson11: flowchart added

--- 011_The_Blackjack/main.py
+++ 011_The_Blackjack/main.py
@@ -2,8 +2,8 @@ from random import randint
 from art import logo
 want_play = True
 
-cards = ["A", "2", "3", "4", "5","6","7","8","9","10","j","q","k","a"]
-values = ['', 2,3,4,5,6,7,8,9,10,10,10,10,""]
+cards = ["A", "2", "3", "4", "5","6","7","8","9","10","j","q","k"]
+values = [11, 2,3,4,5,6,7,8,9,10,10,10,10]
 
 # GAME START
 print(logo)
@@ -23,26 +23,46 @@ def randomize():
     
     return(random_card, random_value)
 
-while want_play == True:
-    play_answer = False
-    while play_answer == False:
-        answer = input("Would you like to play Blackjack?\n Yes (y) or No (n)?:")
-    
-        if answer == "y" or answer == "n":
-            play_answer = True
-        else:
-            print("Invalid answer, please type 'y' or 'n'.")
-        
-    if answer == 'n':
-        want_play = False
-    
-    hand_1 = randomize()
-    hand_2 = randomize()
-    
-    hand = [hand_1[0], hand_2[0]]
-    hand_value = hand_1[1] + hand_2[1]
 
-    print(f" Your card is {hand}, that values {hand_value}")
+while want_play == True:
+    
+    def add_another_number():
+        hand.append(randomize())
+
+    def verifying_input():
+        """Verification of input
+            Returns:
+                Returns if it was typed (y) or (n) correctly.
+        """
+        answer = ""
+        play_answer = False
+        while play_answer == False:
+            answer = input("Would you like to play Blackjack?\n Yes (y) or No (n)?:")
+            if answer == "y" or answer == "n":
+                play_answer = True
+            else:
+                print("Invalid answer, please type 'y' or 'n'.")
+    
+    verifying_input()
+    
+    if verifying_input().answer == "y":
+        
+        hand = [randomize(), randomize()]
+        
+        hand_cards =hand[0,0]
+        hand_value = hand[1,1]
+        
+        enemy = [randomize(), randomize()]
+
+        enemy_cards = enemy[0,0]
+        enemy_value = enemy[1,1]
+
+        print(f" Your cards are {hand_cards}, that values {hand_value}")
+        print(f" Your enemy's cards are {enemy_cards}, that values {enemy_value}")
+    
+    if verifying_input().answer == 'n':
+        want_play = False
+
 
 
 
