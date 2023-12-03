076b7fdc4f772a4b09718254bbfdde7cd5338424
lindotex <lindoten@gmail.com>
2023-12-03 21:17:56 +0000

    attempt 1

--- 014_Higher_Lower_Game/main.py
+++ 014_Higher_Lower_Game/main.py
@@ -2,6 +2,7 @@ from game_data import data
 from art import logo, vs
 import os
 import random
+import time
 
 print("welcome to the")
 print(logo)
@@ -27,6 +28,9 @@ def verification(one, two):
 def game_over():
     return "Game Over."
 
+# def play_game():
+    
+
 answer = str(input("Would you like to play?\nYes(y) or No(n):"))
 
 while answer not in ['y','n']:    
@@ -38,12 +42,13 @@ if answer == 'n':
     print(game_over())
     
 else:
-    print("You got here!")
-    
+    os.system('clear')
     while answer == 'y':
+        os.system('clear')
         COMPARE_ONE = randomize()
         COMPARE_TWO = randomize()
         
+        print(f"Your Score: {SCORE}")
         print(f"\n")
         print(f"-----------------------------")
         print(f"first: {COMPARE_ONE['name']}")
@@ -51,27 +56,28 @@ else:
         print(f"Second: {COMPARE_TWO['name']}")
         print(f"-----------------------------")
         print(f"\n")
-        print(f"Your Score: {SCORE}")
-        print(f"\n")
         GUESS = int(input("Who has more followers?:\nThe first (1) or the second one (2)?:"))
         
         if GUESS in [1,2]:
             right_option = verification(COMPARE_ONE,COMPARE_TWO)
             if GUESS == right_option:
                 SCORE += 1
-                print(f"Your Score: {SCORE}")
                 print("YOU GOT IT RIGHT!")
-                print(f"{COMPARE_ONE['name']} has {COMPARE_ONE['follower_count']}\n{COMPARE_TWO['name']} has {COMPARE_TWO['follower_count']}")
+                print(f"{COMPARE_ONE['name']} has {COMPARE_ONE['follower_count']} Million\n{COMPARE_TWO['name']} has {COMPARE_TWO['follower_count']} Million")
+                time.sleep(5)
                 answer ='y'
+                GUESS = ''
             else:
-                print(f"Your Score: {SCORE}")
                 print("AH\nYou Lose...\nThat is not the right answer.")
-                print(f"{COMPARE_ONE['name']} has {COMPARE_ONE['follower_count']} followers... \n{COMPARE_TWO['name']} has {COMPARE_TWO['follower_count']}!")
+                print(f"{COMPARE_ONE['name']} has {COMPARE_ONE['follower_count']} Million followers... \n{COMPARE_TWO['name']} has {COMPARE_TWO['follower_count']} Million!")
                 print(game_over())
                 answer ='n'
                 print(f"Your final score was: {SCORE} points, congratulations!")
+                GUESS = ''
+                time.sleep(5)
         else:
             print("Your answer is not in the options, please select between 1 or 2.") # FIX THIS!!!!!!!
+            time.sleep(5)
     
 ##### BUG: 
 ##### Make a Loop to fix the part of repetition
