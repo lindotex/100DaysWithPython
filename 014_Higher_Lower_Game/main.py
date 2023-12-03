a7588da91aa273a298df292685de399b4ad7acae
lindotex <lindoten@gmail.com>
2023-12-03 15:59:04 +0000

    bug: fix the last loop

--- 014_Higher_Lower_Game/main.py
+++ 014_Higher_Lower_Game/main.py
@@ -13,9 +13,7 @@ COMPARE_ONE = []
 COMPARE_TWO = []
 SCORE = int()
 GUESS = int()
-
-def play_game(GAME_START):
-    GAME_START = True
+answer = ''
     
 def randomize():
     return random.choice(data)
@@ -26,33 +24,54 @@ def verification(one, two):
     else:
         return 2
 
-def randomize():
-    COMPARE_ONE = randomize()
-    COMPARE_TWO = randomize()
-    name_one = COMPARE_ONE['name']
-    name_two = COMPARE_TWO['name']
-    return "Your score: {SCORE}\n {name_one} \n {vs} \n {name_two}"
+def game_over():
+    return "Game Over."
 
-answer = input("Would you like to play?\nYes(y) or No(n):")
+answer = str(input("Would you like to play?\nYes(y) or No(n):"))
 
-while answer == 'y':
-
-    randomize()
-
-    while GUESS not in [1,2]:
-        GUESS = int(input("Who has more followers?:\nThe first (1) or the second one (2)?:"))
-        if GUESS not in [1,2]:
-            print("Your answer is not in the options.\nPlease type it again!")
+while answer not in ['y','n']:    
+    print("Your answer is not in the options.\nPlease type it again!")
+    answer = str(input("try your answer again...\nPlease choose between Yes (y) or No (n):"))
         
-        right_option = verification(COMPARE_ONE,COMPARE_TWO)
-        if GUESS == right_option:
-            SCORE += 1
-            print("YOU GOT IT RIGHT!")
-            print(f"{COMPARE_ONE['name']} has {COMPARE_ONE}[follower_count].\n{COMPARE_TWO['name']} has {COMPARE_TWO['follower_count']}")
-        else:
-            print("AHHH\nYou Lose...\That is not the right answer.")
-            print(f"{COMPARE_ONE['name']} has {COMPARE_ONE}[follower_count].\n{COMPARE_TWO['name']} has {COMPARE_TWO['follower_count']}")
-            print("Game Over")
-            print(SCORE)
-
+if answer == 'n':
+    print('Ok, see you next time!\nBye Bye!!!')
+    print(game_over())
     
+else:
+    print("You got here!")
+    
+    while answer == 'y':
+        COMPARE_ONE = randomize()
+        COMPARE_TWO = randomize()
+        
+        print(f"\n")
+        print(f"-----------------------------")
+        print(f"first: {COMPARE_ONE['name']}")
+        print(vs)
+        print(f"Second: {COMPARE_TWO['name']}")
+        print(f"-----------------------------")
+        print(f"\n")
+        print(f"Your Score: {SCORE}")
+        print(f"\n")
+        GUESS = int(input("Who has more followers?:\nThe first (1) or the second one (2)?:"))
+        
+        if GUESS in [1,2]:
+            right_option = verification(COMPARE_ONE,COMPARE_TWO)
+            if GUESS == right_option:
+                SCORE += 1
+                print(f"Your Score: {SCORE}")
+                print("YOU GOT IT RIGHT!")
+                print(f"{COMPARE_ONE['name']} has {COMPARE_ONE['follower_count']}\n{COMPARE_TWO['name']} has {COMPARE_TWO['follower_count']}")
+                answer ='y'
+            else:
+                print(f"Your Score: {SCORE}")
+                print("AH\nYou Lose...\nThat is not the right answer.")
+                print(f"{COMPARE_ONE['name']} has {COMPARE_ONE['follower_count']} followers... \n{COMPARE_TWO['name']} has {COMPARE_TWO['follower_count']}!")
+                print(game_over())
+                answer ='n'
+                print(f"Your final score was: {SCORE} points, congratulations!")
+        else:
+            print("Your answer is not in the options, please select between 1 or 2.") # FIX THIS!!!!!!!
+    
+##### BUG: 
+##### Make a Loop to fix the part of repetition
