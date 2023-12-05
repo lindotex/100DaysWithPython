6beeeb4131acee5f4ff7d7ff62f75a0c9e10cea4
lindotex <lindoten@gmail.com>
2023-12-05 16:36:23 +0000

    lesson 15: complete

--- 015_Coffe_Machine/main.py
+++ 015_Coffe_Machine/main.py
@@ -1,13 +1,22 @@
 
 
 from lib import beverages, resources, coins
+import time
 
 # GLOBAL
-money = 0
+option = ''
+
+def presentation():
+    print('Welcome to Coffee Machine Co.')
+    print('\n')
+    print('The best way to keep awake.\n')
 
 def calculate(quarter, dime, nickel, penny):
     return (quarter*coins['quarter'])+(dime*coins['dime'])+(nickel*coins['nickel'])+(penny*coins['penny']) 
 
+def off_machine():
+    print('The machine has been turn off.')
+    
 def compare(paid, price):
     if paid > price:
         return paid - price
@@ -24,20 +33,33 @@ def prepare_coffee(beverage_type, credit, price):
     if coffee_used and water_used and milk_used > 0:
         if credit >= price:
             print('Ok, your Beverage is being prepared...')
+            time.sleep(4)
             print('Here you are!')
+            time.sleep(2)
             print(f"Here is your change: € {round(compare(credit, price), 2)}")
+            time.sleep(2)
+            print("Thank you! Bye bye!")
         else:
             print("Can't be done...")
                  
 
-option = str(input('What would you like ? (Espresso, Latte, Cappuccino):\n'))
-print(f'You chose {option}, it will cost you {beverages[option]['price']}')
-quarters = int(input("How many quarters (0.25)?:"))
-dimes = int(input("How many dimes (0.10)?:"))
-nickels = int(input("How many nickels (0.05)?:"))
-pennies = int(input("How many pennies (0.01)?:"))
+presentation()
+while option not in ['espresso', 'latte', 'cappuccino', 'off']: 
+    option = str(input('What would you like ? (Espresso, Latte, Cappuccino):\n'))
+    if option not in ['espresso', 'latte', 'cappuccino', 'off']:
+        print('This is not into the options!')
 
-credit = calculate(quarters, dimes, nickels, pennies)
+if option == 'off':
+    off_machine()
+    
+else :
+    print(f'You chose {option}, it will cost you € {beverages[option]['price']}')
+    quarters = int(input("How many quarters (0.25)?:"))
+    dimes = int(input("How many dimes (0.10)?:"))
+    nickels = int(input("How many nickels (0.05)?:"))
+    pennies = int(input("How many pennies (0.01)?:"))
 
-print(f"You paid €{round(credit,2)}.")
-prepare_coffee(option, credit, beverages[option]['price'])
+    credit = calculate(quarters, dimes, nickels, pennies)
+
+    print(f"You paid € {round(credit,2)}.")
+    prepare_coffee(option, credit, beverages[option]['price'])
