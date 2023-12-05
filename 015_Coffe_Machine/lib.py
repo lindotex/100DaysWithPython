10c42232a865e909bc3e81df661eed8a4399d7b0
lindotex <lindoten@gmail.com>
2023-12-05 12:35:17 +0000

    lesson 15: progress

--- 015_Coffe_Machine/lib.py
+++ 015_Coffe_Machine/lib.py
@@ -1,26 +1,39 @@
-beverages = [
-    {
-        'name': 'Expresso', 
-        'water': 50, 
-        'coffee':18, 
-        'milk': 0, 'price': 1.50,
+beverages = {
+    'espresso' : {
+        'ingredients': {
+            'water': 50, 
+            'coffee':18, 
+            'milk': 0,
+        }, 
+        'price': 1.50,
     },
-    {
-        'name': 'Latte', 
-        'water': 200,
-        'coffee': 24, 
-        'milk': 150, 
-        'price': 2.50
+    'latte' : {
+        'ingredients': {
+            'water': 200,
+            'coffee': 24, 
+            'milk': 150
+        }, 
+        'price': 2.50,
     },
-    {
-        'name': 'Cappuccino', 
-        'water': 250, 
-        'coffee': 24, 
-        'milk': 100, 
+    'cappuccino': {
+        'ingredients': {
+            'water': 250, 
+            'coffee': 24, 
+            'milk': 100, 
+        },
         'price': 3.00
     }
-]
+}
 
-resources = {'water': 300, 'milk': 200, 'coffee': 100}
+resources = {
+    'water': 300, 
+    'milk': 200, 
+    'coffee': 100
+}
 
-coins = {'penny':0.01, 'dime': 0.10, 'nickel': 0.05, 'quarter': 0.25}
+coins = {
+    'penny':0.01, 
+    'dime': 0.10, 
+    'nickel': 0.05, 
+    'quarter': 0.25, 
+}
--- 015_Coffe_Machine/main.py
+++ 015_Coffe_Machine/main.py
@@ -1,7 +1,43 @@
+
+
 from lib import beverages, resources, coins
 
-print(beverages[0]['price'])
-print(beverages[1]['price'])
-print(beverages[2]['price'])
-print(resources['coffee'])
-print(coins['penny'])
+# GLOBAL
+money = 0
+
+def calculate(quarter, dime, nickel, penny):
+    return (quarter*coins['quarter'])+(dime*coins['dime'])+(nickel*coins['nickel'])+(penny*coins['penny']) 
+
+def compare(paid, price):
+    if paid > price:
+        return paid - price
+    elif paid == price:
+        return 0
+    else:
+        return f'missing € {paid - price}'
+
+def prepare_coffee(beverage_type, credit, price):
+    coffee_used = resources['coffee'] - beverages[beverage_type]['ingredients']['coffee']
+    water_used = resources['water'] - beverages[beverage_type]['ingredients']['water']
+    milk_used = resources['milk'] - beverages[beverage_type]['ingredients']['milk']
+    
+    if coffee_used and water_used and milk_used > 0:
+        if credit >= price:
+            print('Ok, your Beverage is being prepared...')
+            print('Here you are!')
+            print(f"Here is your change: € {round(compare(credit, price), 2)}")
+        else:
+            print("Can't be done...")
+                 
+
+option = str(input('What would you like ? (Espresso, Latte, Cappuccino):\n'))
+print(f'You chose {option}, it will cost you {beverages[option]['price']}')
+quarters = int(input("How many quarters (0.25)?:"))
+dimes = int(input("How many dimes (0.10)?:"))
+nickels = int(input("How many nickels (0.05)?:"))
+pennies = int(input("How many pennies (0.01)?:"))
+
+credit = calculate(quarters, dimes, nickels, pennies)
+
+print(f"You paid €{round(credit,2)}.")
+prepare_coffee(option, credit, beverages[option]['price'])
