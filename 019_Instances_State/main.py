ca7071805e0204de35d9f2a9dfe01822518243fc
lindotex <lindoten@gmail.com>
2024-01-17 10:37:45 +0000

    lesson 19: Start

---
+++ 019_Instances_State/main.py
@@ -0,0 +1,13 @@
+import turtle
+
+tim = turtle.Turtle()
+screen = turtle.Screen()
+
+def move_forward():
+    tim.forward(10)
+    
+screen.listen()
+
+screen.onkey(key='space', fun=move_forward)
+
+screen.exitonclick()
