7e2d5ba2c0528c314318d9f1b2be4c5880525147
lindotex <lindoten@gmail.com>
2024-04-27 09:34:35 +0100

    day 19 complete

--- 019_Instances_State/main.py
+++ 019_Instances_State/main.py
@@ -1,13 +1,32 @@
-import turtle
+from turtle import Turtle, Screen
 
-tim = turtle.Turtle()
-screen = turtle.Screen()
+tim = Turtle()
+screen = Screen()
+
+def clear_screen():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
 
 def move_forward():
     tim.forward(10)
-    
+        
+def move_back():
+    tim.back(10)
+
+def turn_left():
+    tim.left(15)
+
+def turn_right():
+    tim.right(15)
+
 screen.listen()
 
-screen.onkey(key='space', fun=move_forward)
+screen.onkey(key='w', fun=move_forward)
+screen.onkey(key='a', fun=turn_left)
+screen.onkey(key='s', fun=move_back)
+screen.onkey(key='d', fun=turn_right)
+screen.onkey(key='c', fun=clear_screen)
 
 screen.exitonclick()
