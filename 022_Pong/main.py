79c5f6cf9fb74814b83f4221b25816675971dfdc
Alisson Lindote Novais <lindoten@gmail.com>
2025-08-25 16:47:27 +0100

    fix: add commentaries

--- 022_Pong/main.py
+++ 022_Pong/main.py
@@ -4,35 +4,41 @@ from paddle import Paddle
 from ball import Ball
 from score import Score
 
+#  Instances
+# setup the screen
 screen = Screen()
-ball = Ball()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
-
 screen.listen()
+# setup the ball
+ball = Ball()
 
 # Paddle 1
 paddle1 = Paddle((-360,0))
+# Paddle 2
+paddle2 = Paddle((360,0))
+
+# Key bindings - Paddle 1
 screen.onkey(paddle1.up,"w")
 screen.onkey(paddle1.down,"s")
 
-# Paddle 2
-paddle2 = Paddle((360,0))
+# Key bindings - Paddle 2
 screen.onkey(paddle2.up,"Up")
 screen.onkey(paddle2.down,"Down")
 
+# setup the score
 score1 = Score((-260, 260))
 score2 = Score((260, 260))
 
-game_is_on = True
 
+# Game loop
+game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.04)
     ball.move()
-    
     
     # Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
@@ -49,6 +55,5 @@ while game_is_on:
     if ball.xcor() < -380:
         ball.reset_position()  
         score2.increase_score()
-    
-    
+        
 screen.exitonclick()    
